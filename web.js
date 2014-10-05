56589d43cc6edb5e09137b7f1b95fdd35ae7622d
Claudio Wilson <claudiowilson@utexas.edu>
2014-10-04 21:49:55 -0400

    fix typo

--- web.js
+++ web.js
@@ -5,14 +5,18 @@ var express = require('express'),
 
 var app = express();
 app.use(bodyParser.text({type: 'text/plain'}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
-app.post('/interpet/python', function(request, response) {
-	//console.log(request.body);
-	var make = 'echo "' + request.body + '" > test.py';
-	//console.log(make);
+app.post('/interpret/python', function(request, response) {
+	console.log(request.body);
+	var make = 'echo \'' + request.body.stuff + '\' > test.py';
 	var made = exec(make);
+	console.log(make);
 	var command = 'python test.py';
-
+	console.log(command);
 	var child = exec(command, function(error, stdout, stderr) {
 		if (error) {
 			response.status(200).json({"stdout": null, "stderr": null, "error" : error});
